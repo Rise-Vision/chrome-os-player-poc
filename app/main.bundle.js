f6df1a57407ad694028bbbaf37fc3636482df714
André Luiz Cardoso <andrecardoso@gmail.com>
2018-01-31 17:25:48 -0200

    Add output element to show log statements on the window

--- app/main.bundle.js
+++ app/main.bundle.js
@@ -170,22 +170,35 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__filesystem__ = __webpack_require__(0);
 
 
+let output = null;
+
 function testSavingLargeFiles() {
     testSavingLargeFile('http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4', 'bbb_sunflower_1080p_60fps_stereo_abl.mp4');
 }
 
+function writeToOutput(text) {
+    console.log(text);
+    const line = document.createElement("p");
+    line.textContent = text;
+    output.appendChild(line);
+}
+
 function testSavingLargeFile(url, name) {
-    console.log(`Downloading ${url}`);
+    writeToOutput(`Downloading ${url}`);
     fetch(url)
         .then((response) => {
-            console.log(`Saving file ${name}`);
+            writeToOutput(`Saving file ${name}`);
             return __WEBPACK_IMPORTED_MODULE_0__filesystem__["a" /* default */].saveFile(name, response.body);
         })
-        .then((fileUrl) => console.log(`File ${name} saved with success ${fileUrl}`))
-        .catch(console.error);
+        .then((fileUrl) => writeToOutput(`File ${name} saved with success ${fileUrl}`))
+        .catch((error) => {
+            console.error(error);
+            writeToOutput(error);
+        });
 }
 
 function init() {
+    output = document.querySelector('output');
     testSavingLargeFiles();
 }
 
--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 import FileSystem from './filesystem'
 
+let output = null;
+
 function testSavingLargeFiles() {
     testSavingLargeFile('http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4', 'bbb_sunflower_1080p_60fps_stereo_abl.mp4');
 }
 
+function writeToOutput(text) {
+    console.log(text);
+    const line = document.createElement("p");
+    line.textContent = text;
+    output.appendChild(line);
+}
+
 function testSavingLargeFile(url, name) {
-    console.log(`Downloading ${url}`);
+    writeToOutput(`Downloading ${url}`);
     fetch(url)
         .then((response) => {
-            console.log(`Saving file ${name}`);
+            writeToOutput(`Saving file ${name}`);
             return FileSystem.saveFile(name, response.body);
         })
-        .then((fileUrl) => console.log(`File ${name} saved with success ${fileUrl}`))
-        .catch(console.error);
+        .then((fileUrl) => writeToOutput(`File ${name} saved with success ${fileUrl}`))
+        .catch((error) => {
+            console.error(error);
+            writeToOutput(error);
+        });
 }
 
 function init() {
+    output = document.querySelector('output');
     testSavingLargeFiles();
 }
 
